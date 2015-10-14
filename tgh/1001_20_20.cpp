0ead2bcb3e8eb1c1cb62dc2521bbea54fb238ed2
Gwinhen <Gwinhen@Gwinhens-MacBook-Pro.local>
2015-10-14 13:31:28 +0800

    Add comments in codes

--- tgh/1001_20_20.cpp
+++ tgh/1001_20_20.cpp
@@ -10,6 +10,7 @@ int main() {
 	long sum = a + b;
 	int count;
 
+	// Count the number of segments
 	if ((sum / 1000) != 0) {
 		if ((sum / 1000000) != 0) {
 			count = 2;
@@ -23,10 +24,14 @@ int main() {
 		cout << sum;
 	}
 
+	// Remove the symbol except the first segment
 	sum = abs(sum);
+
+	// Print the following segments
 	while (count != 0) {
 		int tmp = (sum / (count == 2? 1000 : 1)) % 1000;
 
+		// Deal with commas and zeros
 		if (tmp >= 100) {
 			cout << "," << tmp;
 		} else if (tmp >= 10) {
--- tgh/1002_25_25.cpp
+++ tgh/1002_25_25.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 using namespace std;
 
+// Struct for each item in polynomial
 struct Poly
 {
-	int index;
-	double coeff;
+	int index;		// Exponent
+	double coeff;	// Coefficient
 };
 
 int main()
@@ -14,6 +15,7 @@ int main()
 	Poly item;
 	vector<Poly> poly1, poly2, poly_sum;
 
+	// Input first polynomial
 	cin >> k;
 
 	for (int i = 0; i < k; i ++) {
@@ -21,6 +23,7 @@ int main()
 		poly1.push_back(item);
 	}
 
+	// Input second polynomial
 	cin >> k;
 
 	for (int i = 0; i < k; i ++) {
@@ -28,26 +31,28 @@ int main()
 		poly2.push_back(item);
 	}
 
+	// Sum up two polynomials
 	for (int i = 0, j = 0; i < poly1.size() || j < poly2.size();) {
-		if (i >= poly1.size()) {
+		if (i >= poly1.size()) {									// The second polynomial has remained items
 			poly_sum.push_back(poly2.at(j++));
-		} else if (j >= poly2.size()) {
+		} else if (j >= poly2.size()) {								// The first polynomial has remained items
 			poly_sum.push_back(poly1.at(i++));
 		} else {
-			if (poly1.at(i).index > poly2.at(j).index) {
+			if (poly1.at(i).index > poly2.at(j).index) {			// Larger exponent of the first one
 				poly_sum.push_back(poly1.at(i++));
-			} else if (poly1.at(i).index == poly2.at(j).index) {
+			} else if (poly1.at(i).index == poly2.at(j).index) {	// Same exponent
 				item.index = poly1.at(i).index;
 				item.coeff = poly1.at(i++).coeff + poly2.at(j++).coeff;
-				if (item.coeff != 0) {
+				if (item.coeff != 0) {								// Ignore the item with zero coefficient
 					poly_sum.push_back(item);
 				}
-			} else if (poly1.at(i).index < poly2.at(j).index) {
+			} else if (poly1.at(i).index < poly2.at(j).index) {		// Larger exponent of the second one
 				poly_sum.push_back(poly2.at(j++));
 			}
 		}
 	}
 
+	// Print results
 	cout << poly_sum.size();
 
 	vector<Poly>::iterator it;
